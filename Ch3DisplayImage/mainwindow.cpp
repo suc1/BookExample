25839642050e29eba1ae9736e89c1eb00560a775
suc1 <ytsucheng@gmail.com>
2017-06-07 11:40:31 -0400

    default draw

--- Ch3DisplayImage/mainwindow.cpp
+++ Ch3DisplayImage/mainwindow.cpp
@@ -12,3 +12,19 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::paintEvent(QPaintEvent* event)
+{
+    QImage image;
+    image.load("checker.png");
+    QImage image2;
+    image2.load("tux.png");
+    QImage image3;
+    image3.load("butterfly.png");
+
+    QPainter painter(this);
+    painter.drawImage(QPoint(10, 10), image);
+    painter.drawImage(QPoint(10, 10), image2);
+    painter.drawImage(QPoint(300, 10), image);
+    painter.drawImage(QPoint(300, 40), image3);
+}
