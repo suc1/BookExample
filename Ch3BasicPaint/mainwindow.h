27674f8bc16382f6eeac9892900376835ccfcdea
suc1 <ytsucheng@gmail.com>
2017-06-08 11:05:58 -0400

    Add draw function

--- Ch3BasicPaint/mainwindow.h
+++ Ch3BasicPaint/mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QPainter>
+#include <QMouseEvent>
+#include <QFileDialog>
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +18,41 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+    virtual void mousePressEvent(QMouseEvent *event);
+    virtual void mouseMoveEvent(QMouseEvent *event);
+    virtual void mouseReleaseEvent(QMouseEvent *event);
+    virtual void paintEvent(QPaintEvent *event);
+    virtual void resizeEvent(QResizeEvent *event);
+
+private slots:
+    void on_actionSave_triggered();
+
+    void on_actionClear_triggered();
+
+    void on_action2px_triggered();
+
+    void on_action5px_triggered();
+
+    void on_action10px_triggered();
+
+    void on_actionBlack_triggered();
+
+    void on_actionWhite_triggered();
+
+    void on_actionRed_triggered();
+
+    void on_actionGreen_triggered();
+
+    void on_actionBlue_triggered();
+
 private:
     Ui::MainWindow *ui;
+
+    QImage image;
+    bool drawing;
+    QPoint lastPoint;
+    int brushSize;
+    QColor brushColor;
 };
 
 #endif // MAINWINDOW_H
